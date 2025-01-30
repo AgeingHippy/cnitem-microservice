b71a5d4abbff36669a609f8954490eeebc5b4848
AgeingHippy <charles_mcdonald@hotmail.com>
2025-01-30 13:49:51 +0000

    Initial commit

---
+++ src/main/java/com/ageinghippy/cnitem_microservice/CnItemMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ageinghippy.cnitem_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnItemMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnItemMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ageinghippy/cnitem_microservice/CnItemMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ageinghippy.cnitem_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnItemMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
